8d9e3efec9a97bcaa3a3b466a02c138857b37438
Ivan Bedov <freginder@mail.ru>
2023-10-22 16:53:18 +0700

    Create User notifications about updating photos

--- app/src/main/java/com/sample/photogallery/PollWorker.kt
+++ app/src/main/java/com/sample/photogallery/PollWorker.kt
@@ -1,7 +1,10 @@
 package com.sample.photogallery
 
+import android.app.PendingIntent
 import android.content.Context
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import kotlinx.coroutines.flow.first
@@ -30,6 +33,7 @@ class PollWorker(
                 } else {
                     Log.i(TAG, "Got a new result: $newResultId")
                     preferencesRepository.setLastResultId(newResultId)
+                    notifyUser()
                 }
             }
             Result.success()
@@ -39,4 +43,26 @@ class PollWorker(
         }
 
     }
+
+    private fun notifyUser() {
+        val intent = MainActivity.newIntent(context)
+        val pendingIntent = PendingIntent.getActivity(
+            context,
+            0,
+            intent,
+            PendingIntent.FLAG_IMMUTABLE
+        )
+        val resources = context.resources
+        val notification = NotificationCompat
+            .Builder(context, NOTIFICATION_CHANNEL_ID)
+            .setTicker(resources.getString(R.string.new_pictures_title))
+            .setSmallIcon(android.R.drawable.ic_menu_report_image)
+            .setContentTitle(resources.getString(R.string.new_pictures_title))
+            .setContentText(resources.getString(R.string.new_pictures_text))
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+            .build()
+        NotificationManagerCompat.from(context).notify(0, notification)
+    }
+
 }
