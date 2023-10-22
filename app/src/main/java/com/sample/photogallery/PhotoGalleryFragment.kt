de7f6cef7df2545873a2021346eb09d73dedd851
Ivan Bedov <freginder@mail.ru>
2023-10-22 12:55:19 +0700

    Update Classes for use Moshi

--- app/src/main/java/com/sample/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/sample/photogallery/PhotoGalleryFragment.kt
@@ -8,11 +8,8 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
-import com.sample.photogallery.api.FlickrApi
 import com.sample.photogallery.databinding.FragmentPhotoGalleryBinding
 import kotlinx.coroutines.launch
-import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
 
 private const val TAG = "PhotoGalleryFragment"
 
