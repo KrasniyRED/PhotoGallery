f4a9cbae4a86f3e53cd749b076f92831eb7645a5
Ivan Bedov <freginder@mail.ru>
2023-10-22 12:34:33 +0700

    Create class PhotoRepository and FlickrApi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -50,6 +50,11 @@ dependencies {
     implementation ("androidx.recyclerview:recyclerview:1.3.2")
     implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.okhttp3:okhttp:4.9.3")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+    implementation ("com.squareup.retrofit2:converter-scalars:2.9.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/sample/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/sample/photogallery/PhotoGalleryFragment.kt
@@ -1,12 +1,20 @@
 package com.sample.photogallery
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
+import com.sample.photogallery.api.FlickrApi
 import com.sample.photogallery.databinding.FragmentPhotoGalleryBinding
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+
+private const val TAG = "PhotoGalleryFragment"
 
 class PhotoGalleryFragment : Fragment() {
     private var _binding: FragmentPhotoGalleryBinding? = null
@@ -24,6 +32,18 @@ class PhotoGalleryFragment : Fragment() {
         binding.photoGrid.layoutManager = GridLayoutManager(context, 3)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            val response =  PhotoRepository().fetchContents()
+            Log.d(TAG, "Response received: $response")
+        }
+
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
