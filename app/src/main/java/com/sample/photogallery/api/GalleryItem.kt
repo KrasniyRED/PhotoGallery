aaa4f8169d043bfbf3f5a6ab47f879172a7dd143
Ivan Bedov <freginder@mail.ru>
2023-11-10 11:15:48 +0700

    Implement Room database

--- app/src/main/java/com/sample/photogallery/api/GalleryItem.kt
+++ app/src/main/java/com/sample/photogallery/api/GalleryItem.kt
@@ -1,12 +1,15 @@
 package com.sample.photogallery.api
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
+@Entity
 data class GalleryItem(
     val title: String,
-    val id: String,
+    @PrimaryKey val id: String,
     @Json(name = "url_s") val url: String,
     )
 
